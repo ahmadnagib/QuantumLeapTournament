0519eaa4555a1bb9228accdf46da01f81a2578db
Ahmad Nagib <ahmadnagib@fci-cu.edu.eg>
2017-07-08 13:12:43 +0200

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -11,9 +11,7 @@ CREATE DATABASE tournament;
 -- create players table
 CREATE TABLE players(
    id serial PRIMARY KEY,
-   name text NOT NULL,
-   win_count integer NOT NULL DEFAULT 0,
-   match_count integer NOT NULL DEFAULT 0
+   name text NOT NULL
 );
 
 -- create matches table
@@ -24,9 +22,27 @@ CREATE TABLE matches(
 	PRIMARY KEY(id, winner, loser)
 );
 
--- create a players standings view out of the players
--- table to be used in tournament.py
+
+-- create a view getting the count of the matches played by each player
+-- by joining players and matches tables
+CREATE VIEW players_match_count AS
+SELECT players.id AS id, players.name AS name, COUNT(matches.id) AS match_count
+FROM players LEFT OUTER JOIN matches
+    ON players.id = matches.loser OR players.id = matches.winner
+GROUP BY players.id;
+
+-- create a view getting the count of the matches won by each player
+-- by joining players and matches tables
+CREATE VIEW players_win_count AS
+SELECT players.id AS id, COUNT(matches.id) AS win_count
+FROM players LEFT OUTER JOIN matches
+    ON players.id = matches.winner
+GROUP BY players.id;
+
+-- create a players standings view by simply joining
+-- the two created views above
 CREATE VIEW players_standings AS
-SELECT *
-FROM players
-ORDER BY win_count DESC;
+SELECT players_match_count.id, players_match_count.name, players_win_count.win_count, players_match_count.match_count
+FROM players_win_count JOIN players_match_count
+    ON players_match_count.id = players_win_count.id
+ORDER BY players_win_count.win_count DESC;
